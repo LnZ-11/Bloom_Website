bc74ea81572ec3a58ab3162002085fa1d3588462
LnZ-11 <zlyes1999@gmail.com>
2025-03-23 19:28:05 +0100

    adding bio label and certifiaction paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,19 @@ export default function App() {
       </div>
       <div className="divider divider-success mx-30 md:mx-40 text-2xl md:divider md:divider-success">Bio, Econo, Ecolo</div>
       <div className="flex flex-col items-center gap-4 mt-12">
+        <h2 className="text-6xl text-center font-bold mt-16 text-[#0b8a43] font-bold">Certifications BIO</h2>
+        <h3 className="text-3xl font-bold mb-8 text-center underline">Nationale et Européenne</h3>
+        <p className="text-3xl mb-8 text-center">Chez Bloom, on ne joue pas avec les étiquettes. Nos boissons sont certifiées bio, ce qui signifie qu’elles sont conçues avec des ingrédients cultivés sans pesticides chimiques, sans OGM et dans le respect de la nature. En gros, que du naturel, rien de douteux. S’énergiser sans compromis, c’est ça l’esprit Bloom !</p>
+        <Image src="/Images/Bio.png" alt="Bio" width={500} height={500} className="rounded-xl shadow-[1px_9px_5px_3px_rgba(224,_225,_91,_0.4)]"/>
+        <Image src="/Images/bioLabel.jpeg" alt="Bio" width={500} height={500} className="rounded-xl shadow-[1px_9px_5px_3px_rgba(224,_225,_91,_0.4)]"/>
+      </div>
+      <div className="divider divider-success mx-30 md:mx-40 text-2xl md:divider md:divider-success">Santé, énergie, vitalité</div>
+      <div className="flex flex-col items-center gap-4 mt-12">
         <h2 className="text-6xl text-center font-bold mt-16 text-[#0b8a43] font-bold">Fun Facts & Anecdotes</h2>
         <h3 className="text-3xl font-bold text-center underline">Car on aime le Peps !</h3>
         <Card image="/Images/BloomJuice.jpg" invert={false}/>
       </div>
-      <div className="divider divider-success mx-30 md:mx-40 text-2xl md:divider md:divider-success">Santé, énergie, vitalité</div>
-      <div className="flex flex-col items-center gap-4 mt-12">
-        <h2 className="text-6xl text-center font-bold mt-16 text-[#0b8a43] font-bold">Certifications BIO</h2>
-        <h3 className="text-3xl font-bold mb-8 text-center underline">Nationale et Européenne</h3>
-        <Image src="/Images/Bio.png" alt="Bio" width={500} height={500} className="rounded-xl shadow-[1px_9px_5px_3px_rgba(224,_225,_91,_0.4)]"/>
-      </div>
+
     </div>
   )
 }
